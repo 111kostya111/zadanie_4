0f697fee48435b6d20f422d4982579f4114fc3a9
111kostya111 <kshaburin@gmail.com>
2023-12-20 23:20:17 +0300

    first commit

---
+++ №1.py
@@ -0,0 +1,29 @@
+import math
+
+try:
+    ugol = float(input("Введите величину угла: "))
+except ValueError:
+    print("Введено неккорктное значение")
+    exit()
+
+vibor = input("Введите единицу изверения (rad или deg): ")
+
+if vibor == "rad":
+    pass
+elif vibor == "deg":
+    ugol = math.radians(ugol)
+else:
+    print("Введено неккорктное значение")
+    exit()
+
+sinx = round(math.sin(ugol), 2)
+cosx = round(math.cos(ugol), 2)
+print("sinx = ", sinx)
+print("cosx = ", cosx)
+
+if math.degrees(ugol) % 90 == 0 and math.degrees(ugol) % 180 != 0:
+    print("ERROR")
+    exit()
+
+tanx = round(math.tan(ugol), 2)
+print("tanx = ", tanx)
